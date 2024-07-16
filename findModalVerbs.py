864ce0d52c60993a5c24200d68c4afa230ab16d7
Linaria2002 <s1290128@u-aizu.ac.jp>
2024-07-16 11:16:13 +0900

    modified fMV

--- findModalVerbs.py
+++ findModalVerbs.py
@@ -1,12 +1,17 @@
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+# 2~4行目はmain.pyで処理するため、後でコメントアウトする
+# import nltk
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
 
+# 助動詞があればそのwordとindexのタプルのリストを返す。
 def findModalVerbs(str):
     morph = nltk.word_tokenize(str)
     tagged = nltk.pos_tag(morph)
-    print(tagged)
-    res = list(filter(lambda x: x[1] == 'MD', tagged))
+    # print(tagged)
+    res = []
+    for i in range(len(tagged)):
+        if tagged[i][1] == "MD":
+            res.append((tagged[i][0], i))
     return res
 
 if __name__ == '__main__':
